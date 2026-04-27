bbd436c3f347aee003164a90680127a4fbdf3fec
Montyyy <danyramadhan558@gmail.com>
2026-04-27 20:17:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,3 +3,14 @@ const multer = require('multer');
 const path = require('path');     
 const app = express();
 const PORT = 3000;
+
+const aturanPenyimpanan = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads'); 
+    },
+    filename: function (req, file, cb) {
+        const namaUnik = Date.now() + path.extname(file.originalname);
+        cb(null, namaUnik);
+    }
+});
+const upload = multer({ storage: aturanPenyimpanan });
